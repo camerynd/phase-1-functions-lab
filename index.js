2a5cb914a3063202974ad7f90e3ac80ec71da960
github username <github email address>
2021-07-20 10:15:21 -0600

    commit tree 49277d92f89292d4079a592fa39694b5f3f0e2d9

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const hqLocation = 42
+function distanceFromHqInBlocks(pickupLocation) {
+    if(pickupLocation > hqLocation) {
+        return pickupLocation - hqLocation
+    } else if(pickupLocation < hqLocation) {
+        return hqLocation - pickupLocation
+    }
+}
+const feetInABlock = 264
+function distanceFromHqInFeet(pickupLocation) {
+    return feetInABlock * distanceFromHqInBlocks(pickupLocation)
+}
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+    if(startingBlock > endingBlock) {
+        return (startingBlock - endingBlock) * feetInABlock
+    } else if(startingBlock < endingBlock) {
+        return (endingBlock - startingBlock) * feetInABlock
+    }
+}
+function calculatesFarePrice(startingBlock, endingBlock) {
+    const feetTravelled = distanceTravelledInFeet(startingBlock, endingBlock)
+    if(feetTravelled <= 400) {
+        return 0
+    } else if(feetTravelled > 400 && feetTravelled < 2000) {
+        return (feetTravelled - 400) * 0.02
+    } else if(feetTravelled >= 2000 && feetTravelled <= 2500) {
+        return 25
+    } else if (feetTravelled > 2500) {
+        return 'cannot travel that far'
+    }
+}
